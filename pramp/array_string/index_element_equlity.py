63869f380eb9b1fb3413e6f3001b2826d5695999
tong.bu <stepsame@gmail.com>
2023-01-05 22:11:04 +0800

    update index element equlity

--- pramp/array_string/index_element_equlity.py
+++ pramp/array_string/index_element_equlity.py
@@ -10,10 +10,18 @@ output: -1 # since no index in arr satisfies arr[i] == i.
 
 """
 Solution:
-We can use binary search to search the array.
-if we found a match, then we need check the item before to find the first match.
+Because the imputed array is sorted, we could use the binary search algorithm to search the answer.
 
-Time Complexity: O(logN) worst case: O(N)
+For every loop in the binary search, we compare the middle element’s value to its index.
+
+There are 4 cases:
+If the value is equal to the index and the previous one is not equal,  then we find the answer.
+If they are equal but the previous one is equal, the answer will be in the left part.
+If the value is less than the index, the answer will be in the right part.
+If the value is greater than the index, the answer will be in the left part, same in case 2.
+
+
+Time Complexity: O(logN) 
 Space Complexity: O(1)
 
 """
@@ -23,14 +31,12 @@ def solution(arr):
     left, right = 0, len(arr) - 1
     while left <= right:
         mid = (left + right) // 2
-        if mid == arr[mid]:
-            # find the first one
-            while mid > 0 and mid - 1 == arr[mid - 1]:
-                mid -= 1
+        if mid == arr[mid] and (mid == 0 or mid - 1 != arr[mid-1]):
             return mid
         if mid > arr[mid]:
             left = mid + 1
         else:
+            # two case: mid < arr[mid]; mid == arr[mid] and mid - 1 == arr[mid-1]
             right = mid - 1
     return -1
 
@@ -38,6 +44,7 @@ def solution(arr):
 def main():
     assert solution([-8,0,2,5]) == 2
     assert solution([-1,0,3,6]) == -1
+    assert solution([0, 1, 2, 3, 4, 5]) == 0
 
 
 if __name__ == '__main__':
